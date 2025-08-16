4afdfe765575cf1b944eeeef9cd6575f268a642b
mudasir najimudin <mudassirnajimuddin@gmail.com>
2025-08-16 18:41:22 +0000

    updated the navbar

--- src/assets/components/Navbar.tsx
+++ src/assets/components/Navbar.tsx
@@ -13,26 +13,24 @@ const Navbar: FC = () => {
   return (
     <nav
       className="fixed top-2 left-5 right-5
-                 rounded-[1.5rem_0_0_1.5rem]
+                 rounded-[5rem_5rem_5rem_5rem]
                  border border-transparent
                  bg-transparent
                  z-50
                  flex flex-col sm:flex-row
                  items-center justify-between
-                 px-6 sm:px-8 md:px-10 lg:px-12
-                 py-3 sm:py-4
-                 shadow-lg
-                 transition-all duration-300"
+                 px-6 sm:px-8 md:px-10 
+                
+                "
       style={{
-        background:
-          "linear-gradient(to right, #DC2626, #00E0FF, #9B5CF6, black)",
+        background: "linear-gradient(black)",
       }}
     >
       {/* Top bar with logo and hamburger */}
-      <div className="flex justify-between items-center w-full  max-w-7xl mx-auto">
+      <div className="flex justify-between items-center w-full max-w-7xl mx-auto">
         {/* Logo Section */}
         <div className="flex items-center space-x-2 sm:space-x-3  md:space-x-4">
-          <div className="w-10 h-10 sm:w-12 sm:h-12 md:w-14 md:h-14">
+          <div className=" w-10 h-10 sm:w-12 sm:h-12 md:w-14 md:h-14">
             <svg
               width="100%"
               height="100%"
@@ -43,22 +41,22 @@ const Navbar: FC = () => {
             >
               <path
                 d="M532.988 229.048C520.015 241.6 507.08 254.075 494.106 266.627C492.641 267.442 488.246 269.696 482.526 268.787C474.566 267.537 470.566 261.217 469.983 260.253C451.559 237.528 433.173 214.725 414.749 191.999C413.855 190.303 411.676 185.56 412.543 179.34C413.637 171.884 418.412 167.506 419.644 166.382C433.159 154.292 446.598 142.163 460.113 130.073C448.108 120.649 375.693 65.8488 276.523 81.914C160.561 100.735 109.067 200.099 104.272 209.768C98.3649 221.68 62.2018 297.518 91.1249 385.973C103.577 424.241 123.792 451.131 137.325 466.612C155.692 438.713 177.227 399.953 193.899 350.782C209.386 305.167 215.966 264.291 218.793 232.54C243.709 233.621 275.494 232.711 311.701 226.187C347.522 219.665 377.421 209.532 400.215 199.922C420.73 224.263 441.207 248.681 461.723 273.022C447.463 293.61 431.103 320.963 417.277 355.065C403.028 390.018 395.566 421.783 391.481 446.934C363.427 443.045 328.657 440.836 288.963 444.088C229.112 448.909 180.576 464.353 146.751 478.322C156.828 488.716 221.977 553.502 323.892 549.4C420.979 545.505 479.239 482.07 490.974 468.711C537.45 415.624 545.902 357.172 547.812 339.712C553.394 289.186 541.052 249.142 533.219 228.97L532.988 229.048Z"
-                fill="black"
+                fill="white"
               />
               <path
                 d="M306.432 356.763C322.97 364.965 343.024 358.211 351.222 341.679C359.421 325.146 352.66 305.095 336.122 296.894C319.583 288.692 299.529 295.445 291.331 311.978C283.132 328.510 289.893 348.561 306.432 356.763Z"
-                fill="black"
+                fill="white"
               />
               <path
                 d="M295.239 347.84L144.998 470"
-                stroke="black"
+                stroke="white"
                 strokeWidth="7"
                 strokeLinecap="round"
                 strokeLinejoin="round"
               />
               <path
                 d="M295.239 347.84L144.998 470"
-                stroke="black"
+                stroke="white"
                 strokeWidth="7"
                 strokeLinecap="round"
                 strokeLinejoin="round"
@@ -78,7 +76,12 @@ const Navbar: FC = () => {
           {isMenuOpen ? (
             <X className="w-6 h-6 sm:w-7 sm:h-7" />
           ) : (
-            <Menu className="w-6 h-6 sm:w-7 sm:h-7" />
+            <Menu
+              className="w-9 h-6 sm:w-7 sm:h-7 
+                
+                 
+                 "
+            />
           )}
         </button>
 
